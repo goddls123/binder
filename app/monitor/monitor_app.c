86cac01a800ccabdf9cad4afc6526edd025e13f4
Lee <coolch0916@gmail.com>
2020-09-08 14:31:34 +0900

    fixed child process handler

--- app/monitor/monitor_app.c
+++ app/monitor/monitor_app.c
@@ -11,26 +11,21 @@
 #include <signal.h>
 #include "binder_ioctl.h"
 
-pid_t child_pid[2];
+pid_t child_pid[4];
+int fd;
 
-void ex_osd(){
+int execute(const char* path,int type){
 
-	child_pid[0] =fork();
+	child_pid[type] =fork();
 
-	if(child_pid[0] ==0){
-		printf("child process\n");
-		execl("../bin/osd","osd",NULL);
+	if(child_pid[type] ==0){
+		close(fd);
+		execl(path, path, NULL);
 	}
-}
-
-void ex_ioman(){
-
-	child_pid[1] =fork();
-
-	if(child_pid[1] ==0){
-		printf("child process\n");
-		execl("../bin/ioman","ioman",NULL);
-	}
+	else if(child_pid[type]>0)
+		printf("osd pid %d\n",child_pid[type]);
+	else
+		return -1;
 }
 
 static void child_handler(int sig){
@@ -42,29 +37,39 @@ static void child_handler(int sig){
 	fprintf(stderr,"CHILD EXIT %d\n",pid);
 
 	fprintf(stderr,"restart\n");	
-	if(pid == child_pid[0])
-		ex_osd();
-	else if(pid == child_pid[1])
-		ex_ioman();
+	if(pid == child_pid[PT_OSD])
+		execute("../bin/osd",PT_OSD);
+	else if(pid == child_pid[PT_IOMAN])
+		execute("../bin/ioman",PT_IOMAN);
+}
+
+static void wait_pid(int sig)
+{
+	int status;
+	pid_t pid = waitpid(-1,&status,WNOHANG);
+
+	if(!pid)
+		return ;
+	fprintf(stderr,"CHILD EXIT %d\n",pid);
 }
 
 static void kill_handler(int sig){
 
-	printf("process\n");
-	printf("killed\n");
-	kill(child_pid[0],SIGKILL);
-	kill(child_pid[1],SIGKILL);
+	signal(SIGCHLD,(void *)wait_pid);
+	kill(child_pid[PT_IOMAN],SIGKILL);
+	sleep(1);
+	kill(child_pid[PT_OSD],SIGKILL);
+	sleep(1);
 	kill(getpid(),SIGINT);
-
 }
 
 int main()
 {
-	int fd;
 	int a = PT_MONITOR;
 	char buffer[16] = {0,};	 
 	int r;
 	pid_t pid;
+	int status;
 
 
 	fd = open("/dev/binder",O_RDWR);
@@ -79,13 +84,17 @@ int main()
 
 	ioctl(fd, IOSET_TYPE, PT_MONITOR);
 	
-	ex_osd();
-	ex_ioman();
+	if(execute("../bin/osd",PT_OSD)<0)
+		return -1;
+	if(execute("../bin/ioman",PT_IOMAN))
+		return -1;
 
 	signal(SIGCHLD,(void *)child_handler);
 	signal(SIGTERM,(void *)kill_handler);
 
 	printf("start\n");
+
+	
 
 	while(1){
 	
